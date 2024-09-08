d8dfb13b76c198cd28200283bc18f5a68bc50998
AnielQueiroz <anielqsilva@gmail.com>
2024-09-08 18:37:16 -0300

    chore: setting routes

--- back/src/app.js
+++ back/src/app.js
@@ -2,10 +2,17 @@ const express = require('express');
 const app = express();
 const port = 3000;
 const authRoutes = require('./routes/authRoutes');
+const roleRoutes = require('./routes/roleRoutes');
+const permissionRoutes = require('./routes/permissionRoutes');
+const professionalRoutes = require('./routes/professionalRoutes');
 
 app.use(express.json());
 
+// Rotas
 app.use('/auth', authRoutes);
+app.use('/role', roleRoutes);
+app.use('/permission', permissionRoutes);
+app.use('/professional', professionalRoutes);
 
 app.listen(port, () => {
     console.log(`App listening on port ${port}`)
