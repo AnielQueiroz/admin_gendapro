d4ad97b752d8dee2b3b6d44e01edaaf06c0b1636
anielqsilva <anielqsilva@gmail.com>
2024-09-18 20:15:18 +0000

    chore: added checkPassword to the login route

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -1,6 +1,21 @@
 const { db } = require('../lib/prisma');
-const bcrypt = require('bcryptjs');
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+
+// Função para verificar a senha fornecida
+async function checkPassword(providedPassword, storedHash) {
+    try {
+        const match = await bcrypt.compare(providedPassword, storedHash);
+        if (match) {
+            return true;
+        } else {
+            return false;
+        }
+    } catch (error) {
+        console.error('Erro ao comparar a senha:', error);
+        return false;
+    }
+}
 
 const login = async (req, res) => {
     const { email, password } = req.body;
@@ -10,22 +25,36 @@ const login = async (req, res) => {
     }
 
     try {
-        const user = await db.professional.findUnique({ where: { email } });
+        const professional = await db.professional.findUnique({
+            where: {
+                email
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                password: true,
+                barbershopId: true
+            }
+        });
 
-        if (!user) {
+        if (!professional) {
             return res.status(401).json({ message: 'Usuário não encontrado!' });
         }
 
         // Aqui você pode verificar a senha se necessário
-        // const isPasswordValid = await bcrypt.compare(password, user.password);
-        // if (!isPasswordValid) {
-        //     return res.status(401).json({ message: 'Senha incorreta!' });
-        // }
+        const isPasswordValid = await checkPassword(password, professional.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Senha incorreta!' });
+        }
 
-        return res.json({ message: 'Login bem-sucedido!', user });
+        // Excluir a senha da resposta
+        const { password: _, ...responseProfessional } = professional;
+
+        return res.json({ message: 'Login bem-sucedido!', responseProfessional });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({ message: 'Erro interno do servidor' });
+        return res.status(500).json({ message: 'Erro interno do servidor', error: error.message });
     }
 }
 
