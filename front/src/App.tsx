0c41a3a80b7b5ad124d9617031d0f5646cebd039
AnielQueiroz <anielqsilva@gmail.com>
2024-11-10 12:53:36 -0300

    chore: configuring routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,31 @@
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import "./App.css";
+import "react-toastify/dist/ReactToastify.css";
+import LoginScreen from "./components/login-screen";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./middlewares/protect-route";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path="/" element={<LoginScreen />} />
+
+          <Route
+            path="/home"
+            element={
+              <ProtectedRoute>
+                <h1>Home</h1>
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
