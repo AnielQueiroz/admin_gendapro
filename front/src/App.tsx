d5e21b102f1b32127df921c8d6ecb703272e9b73
AnielQueiroz <anielqsilva@gmail.com>
2024-11-10 09:44:27 -0300

    chore: configuring the enviroment

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <div>
-      <p>Hello Nenel</p>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
