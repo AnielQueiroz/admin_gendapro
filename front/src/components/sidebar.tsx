bfe17fdfbe677f9ab3963732a285060dd3a511c5
AnielQueiroz <anielqsilva@gmail.com>
2024-11-15 18:14:51 -0300

    chore: ajustando logica para permissoes

--- front/src/components/sidebar.tsx
+++ front/src/components/sidebar.tsx
@@ -1,15 +1,26 @@
-import { FiHome, FiCalendar, FiUser, FiSettings, FiTool, FiMonitor, FiKey, FiUsers } from "react-icons/fi";
-import { GrUserWorker } from "react-icons/gr"
+import {
+  FiHome,
+  FiCalendar,
+  // FiUser,
+  // FiSettings,
+  FiTool,
+  FiMonitor,
+  FiKey,
+  FiUsers,
+} from "react-icons/fi";
+import { GrUserWorker } from "react-icons/gr";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "@/hooks/use-auth";
 import SubMenu from "./sub-menu";
 
 const Sidebar = () => {
   const navigate = useNavigate();
-  const { user } = useAuth();
+  const { employee } = useAuth();
 
   // Verifica se o usuário tem permissao
-  const hasPermission = user?.permissions.includes("admin");
+  const checkPermission = (permission: string) => {
+    return employee?.role.permissions.map((p) => p.name).includes(permission);
+  };
 
   return (
     <aside className="hidden lg:flex lg:flex-col lg:w-64 shadow-lg h-screen p-4">
@@ -17,6 +28,7 @@ const Sidebar = () => {
       <ul className="space-y-4">
         <li>
           <button
+            type="button"
             className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
             onClick={() => navigate("/home")}
           >
@@ -26,6 +38,7 @@ const Sidebar = () => {
         </li>
         <li>
           <button
+            type="button"
             className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
             onClick={() => navigate("/operational/clients")}
           >
@@ -43,6 +56,7 @@ const Sidebar = () => {
         /> */}
         <li>
           <button
+            type="button"
             className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
             onClick={() => navigate("/calendar")}
           >
@@ -52,11 +66,12 @@ const Sidebar = () => {
         </li>
         <li>
           <button
+            type="button"
             className="flex items-center w-full p-2 text-gray-700 hover:bg-gray-100 rounded-lg"
             onClick={() => navigate("/services")}
-            >
-              <FiTool className="mr-3" />
-              <span>Serviços</span>
+          >
+            <FiTool className="mr-3" />
+            <span>Serviços</span>
           </button>
         </li>
         {/* <li>
@@ -78,17 +93,25 @@ const Sidebar = () => {
           </button>
         </li> */}
         {/* Exibe o menu administrativo apenas se o usuário tiver permissão */}
-        {hasPermission && (
-            <SubMenu
-              title="Administrativo"
-              icon={<FiMonitor className="mr-3" />}
-              navigate={navigate}
-              items={[
-                { icon: <GrUserWorker className="mr-3" />, label: "Colaboradores", path: "/admin/employees" },
-                { icon: <FiKey className="mr-3" />, label: "Permissões", path: "/admin/permissions" },
-              ]}
-            />
-          )}
+        {checkPermission("schedule_appointments") && (
+          <SubMenu
+            title="Administrativo"
+            icon={<FiMonitor className="mr-3" />}
+            navigate={navigate}
+            items={[
+              {
+                icon: <GrUserWorker className="mr-3" />,
+                label: "Colaboradores",
+                path: "/admin/employees",
+              },
+              {
+                icon: <FiKey className="mr-3" />,
+                label: "Permissões",
+                path: "/admin/permissions",
+              },
+            ]}
+          />
+        )}
       </ul>
     </aside>
   );
