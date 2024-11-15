1870c4f07b454b0440d1cdaaa4dcd6493474bf0f
AnielQueiroz <anielqsilva@gmail.com>
2024-11-15 18:12:28 -0300

    fix: get nao recebe body

--- back/src/routes/professionalRoutes.js
+++ back/src/routes/professionalRoutes.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const { assignRole, createProfessional, listProfessionals, listProfessionalById } = require('../controllers/professionalController');
-const { checkPermission } = require('../middlewares/authMiddleware');
+const express = require("express");
+const {
+  assignRole,
+  createProfessional,
+  listProfessionals,
+  listProfessionalById,
+} = require("../controllers/professionalController");
+const { checkPermission } = require("../middlewares/authMiddleware");
 
 const router = express.Router();
 
@@ -9,15 +14,14 @@ const router = express.Router();
 router.post("/create", createProfessional);
 
 // Rota para atribuir um role a um profissional (somente administradores)
-router.post("/assign-role", checkPermission('assign_role'), assignRole);
+router.post("/assign-role", checkPermission("assign_role"), assignRole);
 
 // Rota para listar todos os professionais
-router.get('/list', listProfessionals);
+router.post("/list", listProfessionals);
 
 // Rota para listar um colaborador por ID
-router.get('/list/:professional', listProfessionalById);
+router.get("/list/:professional", listProfessionalById);
 
 module.exports = router;
-
 
 module.exports = router;
