87a9a2c4167f9e1a79bff5417a6be37e42988a12
AnielQueiroz <anielqsilva@gmail.com>
2024-11-15 17:01:35 -0300

    chore: retirando prop

--- front/src/components/header.tsx
+++ front/src/components/header.tsx
@@ -1,28 +1,33 @@
-import React from "react";
+import type React from "react";
 import { useDispatch } from "react-redux";
 import { FiLogOut } from "react-icons/fi";
 import { logout } from "@/features/auth/authSlice";
 import { useNavigate } from "react-router-dom";
+import { useAuth } from "@/hooks/use-auth";
 
-interface HeaderProps {
-  userName?: string | null;
-}
+// interface HeaderProps {
+//   userName?: string | null;
+// }
 
-const Header: React.FC<HeaderProps> = ({ userName }) => {
+const Header: React.FC = () => {
+  const { employee } = useAuth();
+  const employeeName = employee?.name;
+  const establishment = employee?.barbershop.name;
+
   const dispatch = useDispatch();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleLogout = () => {
     dispatch(logout());
-    navigate("/")
+    navigate("/");
   };
 
   return (
     <header className="flex items-center justify-between p-4 shadow-md lg:px-8">
-      <h1 className="text-xl font-bold">GendaPro</h1>
+      <h1 className="text-xl font-bold">GendaPro | {establishment}</h1>
       <div className="flex items-center space-x-4">
-        <span className="text-gray-600 hidden lg:inline">{userName}</span>
-        <button onClick={handleLogout}>
+        <span className="text-gray-600 hidden lg:inline">{employeeName}</span>
+        <button type="button" onClick={handleLogout}>
           <FiLogOut className="text-red-500" />
         </button>
       </div>
