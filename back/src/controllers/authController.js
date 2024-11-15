aa9c159786688db7841df3d95635cb4a94b7b4c0
AnielQueiroz <anielqsilva@gmail.com>
2024-11-15 16:06:41 -0300

    fix: corrigindo algumas coisas no back

--- back/src/controllers/authController.js
+++ back/src/controllers/authController.js
@@ -1,76 +1,87 @@
-const { db } = require('../lib/prisma');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const { db } = require("../lib/prisma");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
 // Função para verificar a senha fornecida
 async function checkPassword(providedPassword, storedHash) {
-    try {
-        const match = await bcrypt.compare(providedPassword, storedHash);
-        if (match) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (error) {
-        console.error('Erro ao comparar a senha:', error);
-        return false;
+  try {
+    const match = await bcrypt.compare(providedPassword, storedHash);
+    if (match) {
+      return true;
     }
+
+    return false;
+  } catch (error) {
+    console.error("Erro ao comparar a senha:", error);
+    return false;
+  }
 }
 
 const login = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if (!email || !password) {
-        return res.status(400).json({ message: 'Email e senha obrigatórias!' });
+  if (!email || !password) {
+    return res.status(400).json({ message: "Email e senha obrigatórias!" });
+  }
+
+  try {
+    const professional = await db.professional.findUnique({
+      where: {
+        email,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        password: true,
+        barbershopId: true,
+        roleId: true,
+      },
+    });
+
+    if (!professional) {
+      return res.status(401).json({ message: "Usuário não encontrado!" });
     }
 
-    try {
-        const professional = await db.professional.findUnique({
-            where: {
-                email
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                password: true,
-                barbershopId: true,
-                roleId: true
-            }
-        });
-
-        if (!professional) {
-            return res.status(401).json({ message: 'Usuário não encontrado!' });
-        }
-
-        // Aqui você pode verificar a senha se necessário
-        const isPasswordValid = await checkPassword(password, professional.password);
-        if (!isPasswordValid) {
-            return res.status(401).json({ message: 'Senha incorreta!' });
-        }
-
-        // Payload para o JWT
-        const payload = {
-            id: professional.id,
-            barbershopId: professional.barbershopId,
-            roleId: professional.roleId
-        }
-
-        const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '12h' });
-
-        // Adicionar o token ao cabeçalho da resposta
-        // res.setHeader('Authorization', `Bearer ${token}`)
-
-        // Excluir a senha da resposta
-        const { password: _, ...responseProfessional } = professional;
-
-        return res.json({ message: 'Login bem-sucedido!', token, professional: responseProfessional });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({ message: 'Erro interno do servidor', error: error.message });
+    // Aqui você pode verificar a senha se necessário
+    const isPasswordValid = await checkPassword(
+      password,
+      professional.password
+    );
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: "Senha incorreta!" });
     }
-}
+
+    // Payload para o JWT
+    const payload = {
+      id: professional.id,
+      barbershopId: professional.barbershopId,
+      roleId: professional.roleId,
+    };
+
+    console.log(JWT_SECRET);
+
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "12h" });
+
+    // Adicionar o token ao cabeçalho da resposta
+    // res.setHeader('Authorization', `Bearer ${token}`)
+
+    // Excluir a senha da resposta
+    const { password: _, ...responseProfessional } = professional;
+
+    return res.json({
+      message: "Login bem-sucedido!",
+      token,
+      professional: responseProfessional,
+    });
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ message: "Erro interno do servidor", error: error.message });
+  }
+};
 
 module.exports = { login };
--- back/src/controllers/professionalController.js
+++ back/src/controllers/professionalController.js
@@ -1,146 +1,183 @@
-const { db } = require('../lib/prisma');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
+const { db } = require("../lib/prisma");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
-const saltRounds = parseInt(process.env.SALT_ROUNDS)
+const saltRounds = Number.parseInt(process.env.SALT_ROUNDS);
 
 // Controller para criar um novo profissional
 const createProfessional = async (req, res) => {
-    const { name, email, password, confirmPassword, phone, establishmentId, roleId } = req.body;
+  const {
+    name,
+    email,
+    password,
+    confirmPassword,
+    phone,
+    establishmentId,
+    roleId,
+  } = req.body;
 
-    if (!name || !email || !password || !establishmentId || !phone || !roleId) {
-        return res.status(400).json({ message: 'Todos os campos sao obrigatórios!' });
-    }
+  if (!name || !email || !password || !establishmentId || !phone || !roleId) {
+    return res
+      .status(400)
+      .json({ message: "Todos os campos sao obrigatórios!" });
+  }
 
-    if (password !== confirmPassword) {
-        return res.status(400).json({ message: 'As senhas precisam ser iguais!' });
-    }
+  if (password !== confirmPassword) {
+    return res.status(400).json({ message: "As senhas precisam ser iguais!" });
+  }
 
-    const estabelecimento = await db.barbershop.findUnique({
-        where: {
-            id: establishmentId
-        }
-    })
+  const estabelecimento = await db.barbershop.findUnique({
+    where: {
+      id: establishmentId,
+    },
+  });
 
-    if (!estabelecimento) {
-        return res.status(400).json({ message: 'Estabelecimento não encontrado!' });
-    }
+  if (!estabelecimento) {
+    return res.status(400).json({ message: "Estabelecimento não encontrado!" });
+  }
 
-    // TODO: Criptar a senha
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+  // TODO: Criptar a senha
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 
-    // Checar se existe um profissional com o mesmo email
-    const professionalExists = await db.professional.findUnique({
-        where: {
-            email: email
-        }
-    })
+  // Checar se existe um profissional com o mesmo email
+  const professionalExists = await db.professional.findUnique({
+    where: {
+      email: email,
+    },
+  });
 
-    if (professionalExists) {
-        return res.status(400).json({ message: 'Profissional com esse email já existe!' });
-    }
+  if (professionalExists) {
+    return res
+      .status(400)
+      .json({ message: "Profissional com esse email já existe!" });
+  }
 
-    try {
-        const professional = await db.professional.create({
-            data: {
-                name,
-                email,
-                password: hashedPassword,
-                phone,
-                barbershopId: establishmentId,
-                roleId
-            }
-        });
+  try {
+    const professional = await db.professional.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+        phone,
+        barbershopId: establishmentId,
+        roleId,
+      },
+    });
 
-        return res.status(201).json({ message: 'Profissional criado com sucesso!', professional });
-    } catch (error) {
-        return res.status(400).json({ message: 'Erro ao criar o profissional', error: error.message });
-    }
-}
+    return res
+      .status(201)
+      .json({ message: "Profissional criado com sucesso!", professional });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Erro ao criar o profissional", error: error.message });
+  }
+};
 
 // Controller para listar todos os professionais de um estabelecimento
 const listProfessionals = async (req, res) => {
-    const { establishmentId } = req.body;
+  const { establishmentId } = req.body;
 
-    if (!establishmentId) {
-        return res.status(400).json({ message: 'ID do estabelecimento não fornecido!' });
-    }
+  if (!establishmentId) {
+    return res
+      .status(400)
+      .json({ message: "ID do estabelecimento não fornecido!" });
+  }
 
-    try {
-        const professionals = await db.professional.findMany({
-            where: {
-                barbershopId: establishmentId
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                phone: true,
-                barbershopId: true,
-                roleId: true,
-                role: true
-            }
-        })
+  try {
+    const professionals = await db.professional.findMany({
+      where: {
+        barbershopId: establishmentId,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        phone: true,
+        barbershopId: true,
+        roleId: true,
+        role: true,
+      },
+    });
 
-        return res.status(200).json({ professionals });
-    } catch (error) {
-        return res.status(400).json({ message: 'Erro ao listar os profissionais', error: error.message });
-    }
-}
+    return res.status(200).json({ professionals });
+  } catch (error) {
+    return res.status(400).json({
+      message: "Erro ao listar os profissionais",
+      error: error.message,
+    });
+  }
+};
 
 const listProfessionalById = async (req, res) => {
-    const { professional } = req.params;
-    const { establishmentId } = req.body;
+  const professionalId = req.params.professionalId;
+  const { establishmentId } = req.body;
 
-    if (!establishmentId) {
-        return res.status(400).json({ message: 'ID do estabelecimento não fornecido!' });
-    }
+  if (!establishmentId) {
+    return res
+      .status(400)
+      .json({ message: "ID do estabelecimento não fornecido!" });
+  }
 
-    if (!professional) {
-        return res.status(400).json({ message: 'ID do profissional não fornecido!' });
-    }
+  if (!professionalId) {
+    return res
+      .status(400)
+      .json({ message: "ID do profissional não fornecido!" });
+  }
 
-    try {
-        const professional = await db.professional.findUnique({
-            where: {
-                id: professional,
-                barbershopId: establishmentId
-            },
-            select: {
-                id: true,
-                name: true,
-                email: true,
-                phone: true,
-                barbershopId: true,
-                roleId: true,
-                role: true
-            }
-        })
+  try {
+    const professional = await db.professional.findUnique({
+      where: {
+        id: professionalId,
+        barbershopId: establishmentId,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        phone: true,
+        barbershopId: true,
+        roleId: true,
+        role: true,
+      },
+    });
 
-        return res.status(200).json({ professional });
-    } catch (error) {
-        return res.status(400).json({ message: 'Erro ao listar os profissionais', error: error.message });
-    }
-}
+    return res.status(200).json({ professional });
+  } catch (error) {
+    return res.status(400).json({
+      message: "Erro ao listar os profissionais",
+      error: error.message,
+    });
+  }
+};
 
 // Controller para associar um role a um profissional
 const assignRole = async (req, res) => {
-    const { professionalId, roleId } = req.body;
+  const { professionalId, roleId } = req.body;
 
-    try {
-        const professional = await db.professional.update({
-            where: {
-                id: professionalId
-            },
-            data: {
-                roleId
-            },
-        });
+  try {
+    const professional = await db.professional.update({
+      where: {
+        id: professionalId,
+      },
+      data: {
+        roleId,
+      },
+    });
 
-        return res.status(200).json({ message: 'Role atribuída com sucesso!', professional });
-    } catch (error) {
-        return res.status(400).json({ message: 'Erro ao atribuir a role', error: error.message });
-    }
-}
+    return res
+      .status(200)
+      .json({ message: "Role atribuída com sucesso!", professional });
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ message: "Erro ao atribuir a role", error: error.message });
+  }
+};
 
-module.exports = { assignRole, createProfessional, listProfessionals, listProfessionalById };
+module.exports = {
+  assignRole,
+  createProfessional,
+  listProfessionals,
+  listProfessionalById,
+};
