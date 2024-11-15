2ce94166cf29b9008611e52c6f6e171394f960e4
AnielQueiroz <anielqsilva@gmail.com>
2024-11-15 18:12:46 -0300

    chore: adicionando um id ao delete something

--- front/src/components/delete-something.tsx
+++ front/src/components/delete-something.tsx
@@ -1,41 +1,49 @@
-import React from 'react'
+import type React from "react";
 import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-    DialogTitle,
-} from "@/components/ui/dialog"
-import { Button } from './ui/button';
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Button } from "./ui/button";
 
-interface DeleteProps  {
-    isOpen: boolean;
-    onClose: () => void;
-    onConfirm: () => void;
-    something: string;
+interface DeleteProps {
+  isOpen: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+  something: string;
+  id: string;
 }
-  
-const DeleteSomething: React.FC<DeleteProps> = ({ isOpen, onClose, onConfirm, something }) => {
+
+const DeleteSomething: React.FC<DeleteProps> = ({
+  isOpen,
+  onClose,
+  onConfirm,
+  something,
+  id,
+}) => {
   return (
-    <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>Tem certeza que deseja excluir?</DialogTitle>
-                <DialogDescription>
-                    Essa ação não poderá ser desfeita! Você está excluindo: <strong>{something}</strong>
-                </DialogDescription>
-            </DialogHeader>
-            <div className='flex justify-end gap-2 mt-4'>
-                <Button variant="outline" onClick={onClose}>
-                    Cancelar
-                </Button>
-                <Button variant="destructive" onClick={onConfirm}>
-                    Excluir
-                </Button>
-            </div>
-        </DialogContent>
+    <Dialog key={id} open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Tem certeza que deseja excluir?</DialogTitle>
+          <DialogDescription>
+            Essa ação não poderá ser desfeita! Você está excluindo:{" "}
+            <strong>{something}</strong>
+          </DialogDescription>
+        </DialogHeader>
+        <div className="flex justify-end gap-2 mt-4">
+          <Button variant="outline" onClick={onClose}>
+            Cancelar
+          </Button>
+          <Button variant="destructive" onClick={onConfirm}>
+            Excluir
+          </Button>
+        </div>
+      </DialogContent>
     </Dialog>
-  )
-}
+  );
+};
 
 export default DeleteSomething;
